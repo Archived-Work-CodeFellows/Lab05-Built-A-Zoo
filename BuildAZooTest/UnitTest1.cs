2391775818ac10843758728ab472e35a8e65e7b8
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-12 13:45:26 -0700

    Created project base

---
+++ BuildAZooTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using BuildAZoo;
+
+namespace BuildAZooTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
